42934b549457dd2f4e99500ec8fa8ba5e5f00b8c
duanzhiwei <89340545@qq.com>
2020-04-08 06:27:07 +0800

    'bak'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <a-radio-group @change="onChange" v-model="value">
+        <a-radio-button value="a">Hangzhou</a-radio-button>
+        <a-radio-button value="b">Shanghai</a-radio-button>
+        <a-radio-button value="c">Beijing</a-radio-button>
+        <a-radio-button value="d">Chengdu</a-radio-button>
+      </a-radio-group>
+    </div>
+    <div :style="{ marginTop: '16px' }">
+      <a-radio-group @change="onChange" defaultValue="a">
+        <a-radio-button value="a">Hangzhou</a-radio-button>
+        <a-radio-button value="b" disabled>Shanghai</a-radio-button>
+        <a-radio-button value="c">Beijing</a-radio-button>
+        <a-radio-button value="d">Chengdu</a-radio-button>
+      </a-radio-group>
+    </div>
+    <div :style="{ marginTop: '16px' }">
+      <a-radio-group disabled @change="onChange" defaultValue="a">
+        <a-radio-button value="a">Hangzhou</a-radio-button>
+        <a-radio-button value="b">Shanghai</a-radio-button>
+        <a-radio-button value="c">Beijing</a-radio-button>
+        <a-radio-button value="d">Chengdu</a-radio-button>
+      </a-radio-group>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// https://www.antdv.com/components/radio-cn/
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
