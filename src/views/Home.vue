5f6693c55622dfaf3cfad7d80a76bdc3cede3afb
duanzhiwei <89340545@qq.com>
2020-04-10 09:08:19 +0800

    'bak'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,85 +1,10 @@
 <template>
-  <div>
-	  <div>
-		
-		  <div class="list-4">
-  <a-row>
-    <a-col :xs="{ span: 6, offset: 1 }" :lg="{ span: 6, offset: 2 }">
-		<img src="/static/index-content2-1.png" width="100%" alt="">
-		<img src="" alt="">
-		<div class="font">
-			<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-			<p>￥388.00 <span>原价：999元</span></p>
-		</div>
-	</a-col>
-    <a-col :xs="{ span: 6, offset: 1 }" :lg="{ span: 6, offset: 2 }">
-		<img src="/static/index-content2-1.png" width="100%" alt="">
-		<img src="" alt="">
-		<div class="font">
-			<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-			<p>￥388.00 <span>原价：999元</span></p>
-		</div>
-	</a-col>
-    <a-col :xs="{ span: 6, offset: 1 }" :lg="{ span: 6, offset: 2 }">
-		<img src="/static/index-content2-1.png" width="100%" alt="">
-		<img src="" alt="">
-		<div class="font">
-			<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-			<p>￥388.00 <span>原价：999元</span></p>
-		</div>
-	</a-col>
-    <a-col :xs="{ span: 6, offset: 1 }" :lg="{ span: 6, offset: 2 }">
-		<img src="/static/index-content2-1.png" width="100%" alt="">
-		<img src="" alt="">
-		<div class="font">
-			<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-			<p>￥388.00 <span>原价：999元</span></p>
-		</div>
-	</a-col>
-  </a-row>
-  
-  
-  
-  
-  
-			<a-row type="flex">
-			  <a-col :span="6" :order="1">
-				  <img src="" alt="">
-				  <div class="font">
-				  	<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-				  	<p>￥388.00 <span>原价：999元</span></p>
-				  </div>
-			  </a-col>
-			  <a-col :span="6" :order="2">
-				  <img src="/static/index-content2-1.png" width="100%" alt="">
-				  <img src="" alt="">
-				  <div class="font">
-				  	<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-				  	<p>￥388.00 <span>原价：999元</span></p>
-				  </div>
-			  </a-col>
-			  <a-col :span="6" :order="3">
-				  <!-- <img src="../images/index-content2-1.png" alt=""> -->
-				  <img src="" alt="">
-				  <div class="font">
-				  	<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-				  	<p>￥388.00 <span>原价：999元</span></p>
-				  </div>
-			  </a-col>
-			  <a-col :span="6" :order="4">
-				  <!-- <img src="../images/index-content2-1.png" alt=""> -->
-				  <img src="" alt="">
-				  <div class="font">
-				  	<h1>特聘请交大、矿大、武警学院等院校资深导师任教特聘请交大、矿大</h1>
-				  	<p>￥388.00 <span>原价：999元</span></p>
-				  </div>
-			  </a-col>
-
-			</a-row>
-		  </div>
-	  
-	  </div>
-  </div>
+    <div class="header">
+        <a-row :gutter="[8,8]">
+            <a-col :xs="24" :md="12" :style="{background:'#000'}">col-8</a-col>
+            <a-col :xs="24" :md="12" :style="{background:'#000'}">col-8</a-col>
+        </a-row>
+    </div>
 </template>
 
 <script>
@@ -94,11 +19,14 @@ export default {
 </script>
 
 
-<style>
+<style scoped>
+ .header{
+     max-width: 1200px;
+     margin: 0 auto;
+ }
 
-	
-	
-	
-	
-	
+
+
+
+
 </style>
