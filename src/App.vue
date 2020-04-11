467cf2710f03767e83a47742f0088ade7fc41c02
duanzhiwei <89340545@qq.com>
2020-04-11 19:40:17 +0800

    'bak'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app" :class="[device]">
+        <router-view/>
+    </div>
 </template>
+<script>
 
+    import { deviceEnquire } from '@/plugins/device'
+
+    export default {
+        name: 'app',
+        data(){
+            return {
+                device: ''
+            }
+        },
+        mounted(){
+            deviceEnquire(deviceType => {
+                this.device = deviceType;
+            })
+        },
+        computed: {
+            loading(){
+                return this.$store.state.loading.index;
+            }
+        }
+    }
+</script>
 <style>
-  body,#app{
-    padding: 0;
-    margin: 0;
-  }
+    body,#app{
+        padding: 0;
+        margin: 0;
+    }
 </style>
