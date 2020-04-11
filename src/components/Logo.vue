8f89f8086ca7f7d48aee612a57fa6911cf54ec13
duanzhiwei <89340545@qq.com>
2020-04-11 19:28:11 +0800

    'bak'

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,12 +2,12 @@
   <div class="nav-box">
     <div class="content">
       <a-row :gutter="[8,0]">
-        <a-col :xs="24" :md="9"><span>LOGO</span></a-col>
-        <a-col :xs="24" :md="3" @click="$router.push('/')"><span>首页</span></a-col>
-        <a-col :xs="24" :md="3" @click="$router.push('/healthy')"><span>健康管理师</span></a-col>
-        <a-col :xs="24" :md="3" @click="$router.push('/fireControl')"><span>消防工程师</span></a-col>
-        <a-col :xs="24" :md="3" @click="$router.push('/company')"><span>公司介绍</span></a-col>
-        <a-col :xs="24" :md="3"@click="$router.push('/PersonalClass')">
+        <a-col span="9"><span>LOGO</span></a-col>
+        <a-col span="3" @click="$router.push('/')"><span>首页</span></a-col>
+        <a-col span="3" @click="$router.push('/healthy')"><span>健康管理师</span></a-col>
+        <a-col span="3" @click="$router.push('/fireControl')"><span>消防工程师</span></a-col>
+        <a-col span="3" @click="$router.push('/company')"><span>公司介绍</span></a-col>
+        <a-col span="3"@click="$router.push('/PersonalClass')">
           <span>登录</span>/
           <span>注册</span>
         </a-col>
