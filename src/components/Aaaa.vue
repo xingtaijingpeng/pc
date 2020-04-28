22bb71e2b8d9373d2d328757724695f37a13623c
duanzhiwei <89340545@qq.com>
2020-04-28 17:46:12 +0800

    'bak'

--- src/components/Aaaa.vue
+++ src/components/Aaaa.vue
@@ -9,7 +9,22 @@
 			<a-col span="1"></a-col>
 			<a-col :xs="24" :sm="9" :md="9">
 				<h3 style="font-weight: bolder; line-height: 40px;">每日签到</h3>
-
+				<a-row>
+					<a-col span="18" style="color: #ccc;line-height: 40px;">已连续签到{{signdays*1+Gindex}}天</a-col>
+					<a-col span="6">
+						<a-button :type="btype" :loading="loading" :disabled="bdisabled" icon="edit" @click="qiandaoa">
+							{{words}}
+						</a-button>
+					</a-col>
+				</a-row>
+				<a-row style="margin-top: 10px;">
+					<a-col span="24">
+						<a-steps :current="bcurrent">
+							<a-step title="登录成功" />
+							<a-step :title="btitle" />
+						</a-steps>
+					</a-col>
+				</a-row>
 			</a-col>
 		</a-row>
 	</div>
@@ -18,13 +33,73 @@
 <script>
     import VueHorizontalCalendar from 'vue-horizontal-calendar';
     import { Empty } from 'ant-design-vue';
+    import ARow from "ant-design-vue/es/grid/Row";
     export default {
         name: "aaaa",
+		data(){
+            return {
+                loading: false,
+				btype: 'primary',
+				words: '签到',
+                bdisabled: false,
+                bcurrent: 1,
+				btitle: '待签到',
+                Gindex: 0
+			}
+		},
         beforeCreate() {
             this.simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
         },
+		mounted(){
+            //判断是否签到
+            var now = new Date();
+            var year = now.getFullYear(); //得到年份
+            var month = now.getMonth();//得到月份
+            var date = now.getDate();//得到日期
+
+            let sign = sessionStorage.getItem('qiandao:'+year+month+date)
+
+            if(sign){
+                this.loading = false
+                this.bdisabled = true
+                this.btype = 'dashed'
+                this.words = '已签到'
+                this.btitle = '已签到'
+                this.bcurrent = 2
+			}
+		},
+		computed: {
+            signdays(){
+                return sessionStorage.getItem('signdays') ? sessionStorage.getItem('signdays') : 0;
+			}
+		},
 		components:{
+            ARow,
             VueHorizontalCalendar
+		},
+		methods: {
+            qiandaoa(){
+				this.loading = true
+				setTimeout(()=>{
+                    this.loading = false
+                    this.bdisabled = true
+                    this.btype = 'dashed'
+                    this.words = '已签到'
+                    this.btitle = '已签到'
+                    this.bcurrent = 2
+                    this.Gindex = 1
+
+                    //判断是否签到
+                    var now = new Date();
+                    var year = now.getFullYear(); //得到年份
+                    var month = now.getMonth();//得到月份
+                    var date = now.getDate();//得到日期
+
+                    sessionStorage.setItem('qiandao:'+year+month+date,1)
+                    sessionStorage.setItem('signdays',(this.signdays+1))
+
+                },2000)
+			}
 		}
     }
 </script>
