f13c2f29839fbda002292d3d903359432f3e13e4
duanzhiwei <89340545@qq.com>
2020-05-04 17:55:55 +0800

    'bak'

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,52 +1,56 @@
 <template>
-  <div class="nav-box">
-    <div :class="['content']" v-if="device!='mobile'">
-      <a-row :gutter="[8,0]">
-        <a-col :xs="4" :md="6" @click="$router.push('/')"><img class="headImg-l" v-if="base" :src="base.logo" height="70px" /></a-col>
-          <a-col :xs="0" :md="3" @click="$router.push('/')"><span :class="$route.path == '/' ? 'red' : ''">首页</span></a-col>
-          <a-col :xs="5" :md="4" id="nav_list" @mouseover="selectStyle" @mouseleave="aaaaaa">
-              <span>课程分类</span>
-              <div class="navDown" v-if="navDown">
-                  <div v-for="item in options">
-                      <h1 @click="aaadd(item.id)">{{item.name}}</h1>
-                      <div class="navList">
-                          <span v-for="ite in item.children" @click="aaadd(item.id)">{{ite.name}}</span>
-                      </div>
-                  </div>
-              </div>
-          </a-col>
-          <a-col :xs="5" :md="4" @click="$router.push('/company')"><span :class="$route.path == '/company' ? 'red' : ''">公司介绍</span></a-col>
-          <a-col :xs="5" :md="4" @click="tocenter" :class="choose == 5 ? 'red' : ''"><span>个人中心</span></a-col>
-          <a-col :xs="5" :md="3">服务热线：1111111111</a-col>
-      </a-row>
+    <div>
+        <div :style="device=='mobile'? {height: '98px'}:{ height: 0}"></div>
+        <div class="nav-box" :style="device=='mobile'? {position: 'fixed',top:'0',zIndex:2}:{}">
+            <div :class="['content']" v-if="device!='mobile'">
+                <a-row :gutter="[8,0]">
+                    <a-col :xs="4" :md="6" @click="$router.push('/')"><img class="headImg-l" v-if="base" :src="base.logo" height="70px" /></a-col>
+                    <a-col :xs="0" :md="3" @click="$router.push('/')"><span :class="$route.path == '/' ? 'red' : ''">首页</span></a-col>
+                    <a-col :xs="5" :md="4" id="nav_list" @mouseover="selectStyle" @mouseleave="aaaaaa">
+                        <span>课程分类</span>
+                        <div class="navDown" v-if="navDown">
+                            <div v-for="item in options">
+                                <h1 @click="aaadd(item.id)">{{item.name}}</h1>
+                                <div class="navList">
+                                    <span v-for="ite in item.children" @click="aaadd(item.id)">{{ite.name}}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a-col>
+                    <a-col :xs="5" :md="4" @click="$router.push('/company')"><span :class="$route.path == '/company' ? 'red' : ''">公司介绍</span></a-col>
+                    <a-col :xs="5" :md="4" @click="tocenter" :class="choose == 5 ? 'red' : ''"><span>个人中心</span></a-col>
+                    <a-col :xs="5" :md="3">服务热线：1111111111</a-col>
+                </a-row>
+            </div>
+            <div :class="['content']" v-else>
+                <a-row :gutter="[8,0]">
+                    <a-col span="16" @click="$router.push('/')"><img class="headImg-l" v-if="base" :src="base.logo" height="70px" /></a-col>
+                    <a-col span="8">
+                        <a-dropdown :trigger="['click']">
+                            <a class="ant-dropdown-link" @click="e => e.preventDefault()" style="color: #2e3033 !important;">
+                                导航菜单 <a-icon type="down" />
+                            </a>
+                            <a-menu slot="overlay">
+                                <a-menu-item key="111">
+                                    <a @click="$router.push('/')">首页</a>
+                                </a-menu-item>
+                                <a-menu-item :key="item.id" v-for="item in options">
+                                    <a @click="$router.push('/healthy/'+item.id)">{{item.name}}</a>
+                                </a-menu-item>
+                                <a-menu-item key="444">
+                                    <a @click="$router.push('/company')">公司介绍</a>
+                                </a-menu-item>
+                                <a-menu-item key="1">
+                                    <a @click="tocenter">个人中心</a>
+                                </a-menu-item>
+                            </a-menu>
+                        </a-dropdown>
+                    </a-col>
+                </a-row>
+            </div>
+        </div>
     </div>
-      <div :class="['content']" v-else>
-          <a-row :gutter="[8,0]">
-              <a-col span="16" @click="$router.push('/')"><img class="headImg-l" v-if="base" :src="base.logo" height="70px" /></a-col>
-              <a-col span="8">
-                  <a-dropdown :trigger="['click']">
-                      <a class="ant-dropdown-link" @click="e => e.preventDefault()" style="color: #2e3033 !important;">
-                          导航菜单 <a-icon type="down" />
-                      </a>
-                      <a-menu slot="overlay">
-                          <a-menu-item key="111">
-                              <a @click="$router.push('/')">首页</a>
-                          </a-menu-item>
-                          <a-menu-item :key="item.id" v-for="item in options">
-                              <a @click="$router.push('/healthy/'+item.id)">{{item.name}}</a>
-                          </a-menu-item>
-                          <a-menu-item key="444">
-                              <a @click="$router.push('/company')">公司介绍</a>
-                          </a-menu-item>
-                          <a-menu-item key="1">
-                              <a @click="tocenter">个人中心</a>
-                          </a-menu-item>
-                      </a-menu>
-                  </a-dropdown>
-            </a-col>
-          </a-row>
-      </div>
-  </div>
+
 </template>
 
 <script>
